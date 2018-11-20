8186759ef6d6aa8aed47a48764c8a4a81247367b
Kat Perreira <kat.perreira@gmail.com>
2018-11-19 20:12:58 -0800

    got keyboard working

--- noise.js
+++ noise.js
@@ -1,3 +1,27 @@
+
+
+const clickPlay = () => {
+  $('.instrument').on( 'click', 'button', function() {
+
+    const clickNote = $(this).html();
+
+    document.getElementById(`${clickNote}Audio`).play();
+
+  });
+}
+
+const typePlay = () => {
+
+  $('body').keydown(function(event) {
+    const eventNote = String.fromCharCode(event.keyCode).toLowerCase();
+
+    document.getElementById(`${eventNote}Audio`).play();
+
+  });
+}
+
 $(document).ready( function() {
-  // your code here
+
+  clickPlay();
+  typePlay();
 });
